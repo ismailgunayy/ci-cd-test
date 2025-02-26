484bf262f1fc0d9f2abcbd26caa455c860459e10
İsmail Günay <ismailggunay@gmail.com>
2025-02-26 19:21:58 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Friend!</h1>");
+  res.send("<h1>Hello dost!</h1>");
 });
 
 const PORT = 8080;
